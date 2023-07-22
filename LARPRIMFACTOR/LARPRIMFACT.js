51465005f9a9f35dc14a7936921465d2c67a1c25
masterTrooperMx <mastertrooper@gmail.com>
2023-07-21 20:50:51 -0600

    by using primes table I created

--- LARPRIMFACTOR/LARPRIMFACT.js
+++ LARPRIMFACTOR/LARPRIMFACT.js
@@ -24,10 +24,10 @@ function prepareInput() {
 function main() {
     prepareInput();
     console.log(`${input.length} lines`);
-    let maxP = input, length
+    let maxP = input.length;
     for (let i = 0; i < maxP; i++) {
         let prime = Number(input.splice(0, 1)[0]);
-        if (max % prime == 0) { // it is a factor
+        if (num % prime == 0) { // it is a factor
             arr.push(prime);
             console.log(`${prime}`);
         }
